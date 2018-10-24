cc2b0096543bcf25de49754395ea250f075ae025
haoShen3 <40795259+haoShen3@users.noreply.github.com>
2018-10-24 13:00:49 +1100

    Add files via upload

---
+++ Graphs/Graph.h
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+
+typedef struct GraphRep *Graph;
+
+// vertices are ints
+typedef int int;
+
+// edges are pairs of vertices (end-points)
+typedef struct Edge {
+   int v;
+   int w;
+} Edge;
+
+Graph newGraph(int);
+int   numOfVertices(Graph);
+void  insertEdge(Graph, Edge);
+void  removeEdge(Graph, Edge);
+bool  adjacent(Graph, int, int);
+void  showGraph(Graph);
+void  freeGraph(Graph);
