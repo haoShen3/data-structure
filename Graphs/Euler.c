2205535596f41c07499bbb2ac19072302c7a4ef0
haoShen3 <40795259+haoShen3@users.noreply.github.com>
2018-10-30 16:00:17 +1100

    Update Euler.c

--- Graphs/Euler.c
+++ Graphs/Euler.c
@@ -1,5 +1,11 @@
 #include "Graph.c"
 
+"""
+Euler path problem: ﬁnd a path connecting two vertices v,w in graph G.such that the path includes each edge exactly once 
+Theorem:  A graph has an Euler circuit if and only if it is connected and all vertices have even degree 
+Theorem.  A graph has a non-circuitous Euler path if and only if it is connected and exactly two vertices have odd degree
+"""
+
 bool hasEulerPath(Graph g, int src, int dest){
 	if (src != dest){
 		if (degree(g, src) % 2 == 0 || degree(g, dest) % 2 == 0){
